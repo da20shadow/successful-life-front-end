ab64863cb621053794c0e1b3b716fe414dffb875
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-06 06:41:07 +0200

    added user store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from "react-router-dom";
+import App from './App';
+import {default as userStore} from './Features/User/Store/store';
+import './assets/styles/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={userStore}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

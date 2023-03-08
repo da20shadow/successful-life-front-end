a9dadc62f215ae0d3eb5b2cca31fd1010aa38df3
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-08 19:22:56 +0200

    created task list

---
+++ src/Features/Task/components/layout/TasksList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import TaskTableRow from "../common/TaskTableRow";
+
+function TasksList({tasks}){
+    return (
+        <table className="table-auto w-full">
+            <thead>
+            <tr className={'bg-white dark:bg-[#364962]'}>
+                <th className="py-4 uppercase text-center w-[45%]">Task Title</th>
+                <th className="py-4 uppercase text-center w-[15%]">Status</th>
+                <th className="py-4 uppercase text-center w-[10%]">Priority</th>
+                <th className="py-4 uppercase text-center w-[15%]">Start Date</th>
+                <th className="py-4 uppercase text-center w-[15%]">End Date</th>
+            </tr>
+            </thead>
+            <tbody>
+            {tasks.map((task) => (
+                <TaskTableRow key={task.id} task={task} />
+            ))}
+            </tbody>
+        </table>
+    )
+}
+export default TasksList;

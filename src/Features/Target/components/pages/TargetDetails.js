08d9733e6b0b90663f46c6d78f2988775ed0a580
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-09 19:47:00 +0200

    updated target details

--- src/Features/Target/components/pages/TargetDetails.js
+++ src/Features/Target/components/pages/TargetDetails.js
@@ -8,11 +8,19 @@ import PlaylistAddCheckOutlinedIcon from "@mui/icons-material/PlaylistAddCheckOu
 import FlagIcon from '@mui/icons-material/Flag';
 import TextSnippetIcon from "@mui/icons-material/TextSnippet";
 import TasksList from "../../../Task/components";
+import {CKERichTextEditor} from "../../../../components";
+import {Button, Dialog, Fab, Slide} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 function TargetDetails() {
 
     const {targetId} = useParams();
 
+    const [editDescription, setEditDescription] = useState(false);
 
     //Set states for adding Task modal
     const [openAddTaskModal, setOpenAddTaskModal] = useState(false);
@@ -28,6 +36,8 @@ function TargetDetails() {
                 title: 'Task zero!',
                 status: 'In Progress',
                 priority: 'High',
+                urgent:true,
+                important: true,
                 startDate: '2023-03-01 15:30:00',
                 endDate: '2023-04-09 18:30:00'
             },
@@ -35,7 +45,9 @@ function TargetDetails() {
                 id: 2,
                 title: 'Task one!',
                 status: 'To Do',
-                priority: 'Normal',
+                priority: 'Medium',
+                urgent:true,
+                important: false,
                 startDate: '2023-03-01 15:30:00',
                 endDate: '2023-04-09 18:30:00'
             },
@@ -44,6 +56,8 @@ function TargetDetails() {
                 title: 'Task two!',
                 status: 'In Revision',
                 priority: 'Low',
+                urgent:false,
+                important: false,
                 startDate: '2023-03-01 15:30:00',
                 endDate: '2023-04-09 18:30:00'
             },
@@ -52,6 +66,8 @@ function TargetDetails() {
                 title: 'Task three!',
                 status: 'Completed',
                 priority: 'No Priority',
+                urgent:false,
+                important: false,
                 startDate: '2023-03-01 15:30:00',
                 endDate: '2023-04-09 18:30:00'
             },
@@ -75,7 +91,7 @@ function TargetDetails() {
 
     const priorityArr = [
         {name: 'High', icon: <FlagIcon color={'error'}/>},
-        {name: 'Normal', icon: <FlagIcon color={'warning'}/>},
+        {name: 'Medium', icon: <FlagIcon color={'warning'}/>},
         {name: 'Low', icon: <FlagIcon color={'info'}/>},
         {name: 'No Priority', icon: <FlagIcon color={'disabled'}/>},
     ];
@@ -96,6 +112,9 @@ function TargetDetails() {
         console.log('Task ID: ', taskId)
         console.log('New End Date: ', newEndDate)
     }
+    const handleCloseEditor = () => {
+        setEditDescription(false);
+    }
     return (
         <>
             {/*Target TITLE*/}
@@ -104,15 +123,25 @@ function TargetDetails() {
             </h1>
 
             {/*Target Details*/}
-            <div className="m-5 px-4 py-6 bg-white dark:bg-gray-800 border rounded shadow">
+            <div className="m-1 md:m-5 px-4 py-6 bg-white dark:bg-gray-800 border rounded shadow">
 
                 {/*Target Progress bar & daily target info*/}
                 <ProgressBar bgColor={targetProgressBgColor} progress={progressPercentage}/>
 
                 <h4 className={'mt-5 text-xl text-gray-700 dark:text-gray-300'}>
-                    <TextSnippetIcon/> Target Description</h4>
-                <div className={'my-5 p-2 md:p-5 border'}>
-                    {target.description}
+                    <TextSnippetIcon/> Target Description
+                </h4>
+
+                <div onDoubleClick={()=>setEditDescription(true)}
+                     className={'my-5 p-2 md:p-5 border'}>
+
+                    {/*TODO: create my own rich text editor*/}
+                    {editDescription
+                        ? (<><CKERichTextEditor initialValue={target.description} />
+                            <Button onClick={()=>setEditDescription(false)}>Close Editor</Button></>)
+                        : target.description }
+
+
                 </div>
 
                 <div className="flex flex-wrap items-center justify-between">
@@ -135,8 +164,28 @@ function TargetDetails() {
             </div>
 
             {/*Tasks List*/}
-            <TasksList tasks={target.tasks} />
+            <section className={'m-1 md:my-10 md:mx-5'}>
+                <TasksList tasks={target.tasks} />
+            </section>
 
+            {/*Add Task Button*/}
+            <Fab sx={{position: 'fixed', bottom: 65, right: 16}}
+                 onClick={() => setOpenAddTaskModal(true)}
+                 color="primary" aria-label="add">
+                <AddIcon/>
+            </Fab>
+
+            <Dialog
+                fullWidth
+                open={openAddTaskModal}
+                TransitionComponent={Transition}
+                onClose={() => setOpenAddTaskModal(false)}
+                aria-describedby="add-idea-modal"
+            >
+                <h2>Add New Goal Task</h2>
+                <p>TODO: create add goal task form</p>
+
+            </Dialog>
 
         </>
     )

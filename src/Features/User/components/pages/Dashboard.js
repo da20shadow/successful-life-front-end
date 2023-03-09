8651d512bfc13c952b2cb92c875458e674056fb3
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-09 19:47:40 +0200

    updated dashboard

--- src/Features/User/components/pages/Dashboard.js
+++ src/Features/User/components/pages/Dashboard.js
@@ -4,29 +4,33 @@ import AddTaskIcon from "@mui/icons-material/AddTask";
 import TipsAndUpdatesIcon from "@mui/icons-material/TipsAndUpdates";
 import {Dialog, Slide, SpeedDial, SpeedDialAction, SpeedDialIcon} from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+import NewReleasesIcon from '@mui/icons-material/NewReleases';
+import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import {AddIdeaForm} from "../../../Ideas/components";
+import {NavLink} from "react-router-dom";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
-function Dashboard({ user }) {
+function Dashboard({user}) {
     //Set states for Goal, Idea and Task modals
     const [openAddTaskModal, setOpenAddTaskModal] = useState(false);
     const [openAddIdeaModal, setOpenAddIdeaModal] = useState(false);
     const [openAddGoalModal, setOpenAddGoalModal] = useState(false);
 
-    const goals = [
-        { label: 'Urgent Tasks', count: 3 },
-        { label: 'Important Tasks', count: 8 },
-        { label: 'Ideas', count: 2 },
-        { label: 'Achievements', count: 10 },
-    ];
+    const boxesInfo = {
+        urgentTasks: 3,
+        importantTasks: 5,
+        ideas: 13,
+        achievements: 13,
+    };
 
     const tasks = [
-        { id: 1, title: 'Task 1', status: 'In Progress', dueDate: '2022-03-01' },
-        { id: 2, title: 'Task 2', status: 'Not Started', dueDate: '2022-03-05' },
-        { id: 3, title: 'Task 3', status: 'Completed', dueDate: '2022-03-03' }
+        {id: 1, title: 'Task 1', status: 'In Progress', dueDate: '2022-03-01'},
+        {id: 2, title: 'Task 2', status: 'Not Started', dueDate: '2022-03-05'},
+        {id: 3, title: 'Task 3', status: 'Completed', dueDate: '2022-03-03'}
     ];
 
     //Add new goal,idea or task actions
@@ -47,39 +51,84 @@ function Dashboard({ user }) {
         }
     };
 
+    const boxCardStyle = `p-5 text-center cursor-pointer bg-white hover:bg-gray-50 dark:bg-gray-800 dark:hover:bg-[#283445] 
+    shadow hover:shadow-lg hover:-translate-y-1 active:scale-[0.95] transition duration-500 ease-in-out 
+    border border-transparent hover:border-gray-300 hover:dark:border-gray-300 rounded-md`;
+
     return (
         <>
             <div className="container mx-auto">
+
                 <h1 className="text-gray-700 dark:text-gray-300 text-3xl md:text-4xl lg:text-5xl font-bold mb-8 text-center">
                     Welcome, {user.firstName}!
                 </h1>
-                <div className="mb-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-                    {goals.map((goal) => (
-                        <div
-                            key={goal.label}
-                            className="bg-white dark:bg-gray-600 shadow-md p-8 rounded-md flex flex-col justify-center items-center"
-                        >
-                            <p className="text-gray-700 dark:text-blue-300 text-3xl font-bold">{goal.count}</p>
-                            <h4 className="text-gray-700 dark:text-gray-300 text-lg mb-4">{goal.label}</h4>
+
+                {/*TODO: add links for each to redirect to urgent/important tasks ideas etc*/}
+                <div className="mb-20 mx-1 md:mx-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+                    <NavLink to={'/urgent-tasks'}>
+                        <div className={boxCardStyle}>
+                            <div className="flex gap-3 justify-center items-center">
+                                <LocalFireDepartmentIcon sx={{fontSize: '55px', color: '#da7326'}}/>
+                                <div>
+                                    <p className="text-gray-700 dark:text-blue-300 text-3xl font-bold">{boxesInfo.urgentTasks}</p>
+                                    <h4 className="whitespace-nowrap text-gray-700 dark:text-gray-300 text-lg mb-4">Urgent Tasks</h4>
+                                </div>
+                            </div>
                         </div>
-                    ))}
+                    </NavLink>
+                    <NavLink to={'/important-tasks'}>
+                        <div className={boxCardStyle}>
+                            <div className="flex gap-3 justify-center items-center">
+                                <NewReleasesIcon sx={{fontSize: '55px', color: '#ecc06e'}}/>
+                                <div>
+                                    <p className="text-gray-700 dark:text-blue-300 text-3xl font-bold">{boxesInfo.importantTasks}</p>
+                                    <h4 className="whitespace-nowrap text-gray-700 dark:text-gray-300 text-lg mb-4">Important Tasks</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </NavLink>
+                    <NavLink to={'/ideas'}>
+                        <div className={boxCardStyle}>
+                            <div className="flex gap-3 justify-center items-center">
+                                <TipsAndUpdatesIcon sx={{fontSize: '55px', color: '#ecc06e'}}/>
+                                <div>
+                                    <p className="text-gray-700 dark:text-blue-300 text-3xl font-bold">{boxesInfo.ideas}</p>
+                                    <h4 className="whitespace-nowrap text-gray-700 dark:text-gray-300 text-lg mb-4"> Ideas</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </NavLink>
+                    <NavLink to={'/ideas'}>
+                        <div className={boxCardStyle}>
+                            <div className="flex gap-3 justify-center items-center">
+                                <EmojiEventsIcon sx={{fontSize: '55px', color: '#ecc06e'}}/>
+                                <div>
+                                    <p className="text-gray-700 dark:text-blue-300 text-3xl font-bold">{boxesInfo.achievements}</p>
+                                    <h4 className="whitespace-nowrap text-gray-700 dark:text-gray-300 text-lg mb-4">Achievements</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </NavLink>
                 </div>
 
                 <h2 className={'text-gray-700 dark:text-rose-100 text-3xl font-bold text-center'}>Urgent tasks!</h2>
 
+                {/*Urgent tasks*/}
                 <table className="mt-5 w-full">
                     <thead>
                     <tr>
                         <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Title</th>
                         <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
-                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Due Date</th>
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Due
+                            Date
+                        </th>
                     </tr>
                     </thead>
                     <tbody>
                     {tasks.map((task, index) => (
-                        <tr key={task.id} className={`${index % 2 === 0 
-                            ? 'dark:bg-gray-700 bg-gray-200' 
-                            : 'dark:bg-gray-700 bg-gray-100' } dark:hover:bg-gray-900 bg-gray-300`}>
+                        <tr key={task.id} className={`${index % 2 === 0
+                            ? 'dark:bg-gray-700 bg-gray-200'
+                            : 'dark:bg-gray-700 bg-gray-100'} dark:hover:bg-gray-900 bg-gray-300`}>
                             <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{task.title}</td>
                             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{task.status}</td>
                             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{task.dueDate}</td>

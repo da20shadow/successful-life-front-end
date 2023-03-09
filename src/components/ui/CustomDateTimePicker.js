3045e316a2b4bdd8fd0d63d4080a4ef18fcbd07e
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-09 19:46:05 +0200

    updated date time picker

--- src/components/ui/CustomDateTimePicker.js
+++ src/components/ui/CustomDateTimePicker.js
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import {useState} from "react";
-import './CustomDateTimePicker.css';
 
 function CustomDateTimePicker({ label, inputValue, onChange}){
 
-    const [date, setDate] = useState('');
-    const [time, setTime] = useState('');
+    const [date, setDate] = useState(inputValue ? inputValue.substr(0, 10) : '');
+    const [time, setTime] = useState(inputValue ? inputValue.substr(11, 5) : '');
 
     const handleDateChange = (e) => {
         setDate(e.target.value);
@@ -14,13 +13,17 @@ function CustomDateTimePicker({ label, inputValue, onChange}){
 
     const handleTimeChange = (e) => {
         setTime(e.target.value);
+        if (date === '') {
+            const today = new Date();
+            const year = today.getFullYear();
+            const month = `${today.getMonth() + 1}`.padStart(2, '0');
+            const day = `${today.getDate()}`.padStart(2, '0');
+            return onChange(`${year}-${month}-${day} ${e.target.value}`);
+        }
         onChange(`${date} ${e.target.value}`);
     };
 
-
-
     return (
-
             <div className={'mb-3 flex items-center'}>
                 <input
                     type="date"
@@ -28,7 +31,7 @@ function CustomDateTimePicker({ label, inputValue, onChange}){
                     name="date"
                     value={date}
                     onChange={handleDateChange}
-                    className="bg-gray-200 dark:bg-gray-800 border border-gray-300 rounded-l px-3 py-2 "
+                    className="bg-gray-200 dark:bg-gray-800 text-gray-700 dark:text-gray-300 border border-gray-300 rounded-l px-3 py-2 "
                 />
                 <input
                     type="time"
@@ -36,10 +39,9 @@ function CustomDateTimePicker({ label, inputValue, onChange}){
                     name="time"
                     value={time}
                     onChange={handleTimeChange}
-                    className="bg-gray-200 dark:bg-gray-800 border border-gray-300 rounded-r px-3 py-2 "
+                    className="bg-gray-200 dark:bg-gray-800 text-gray-700 dark:text-gray-300 border border-gray-300 rounded-r px-3 py-2 "
                 />
             </div>
-
     );
 }
 export default CustomDateTimePicker;

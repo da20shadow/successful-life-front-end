cc58e85913359682f0a8a1b1687e2abc525a3c9e
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-10 10:50:48 +0200

    modal style updated

--- src/Features/Ideas/components/common/IdeaCard.js
+++ src/Features/Ideas/components/common/IdeaCard.js
@@ -43,18 +43,19 @@ function IdeaCard({idea}) {
         <>
             <div
                 className={ideaCardStyle}>
-                <h2 onClick={()=>setOpenViewIdeaModal(true)} className="cursor-pointer text-gray-700 dark:text-gray-300 text-xl font-bold border-b mb-4">{idea.title}</h2>
+                <h2 onClick={() => setOpenViewIdeaModal(true)}
+                    className="cursor-pointer text-gray-700 dark:text-gray-300 text-xl font-bold border-b mb-4">{idea.title}</h2>
                 <p className="text-gray-600 dark:text-gray-400 mb-4">{idea.description}</p>
                 <div className="flex flex-wrap gap-2">
                     {idea.tags.map((tag) => (
                         <span key={tag}
-                            className="px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-md text-gray-600 dark:text-gray-300 text-sm"
+                              className="px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-md text-gray-600 dark:text-gray-300 text-sm"
                         >
             {tag}
           </span>
                     ))}
 
-                    <DeleteIcon onClick={()=> setOpenDeleteModal(true)}
+                    <DeleteIcon onClick={() => setOpenDeleteModal(true)}
                                 className={'absolute cursor-pointer text-rose-400 hover:text-rose-700 dark:text-rose-200 hover:dark:text-rose-300 bottom-2 right-1'}/>
 
                 </div>
@@ -68,24 +69,24 @@ function IdeaCard({idea}) {
                 aria-describedby="alert-dialog-slide-description"
             >
 
-                <DialogTitle className={'text-center text-rose-900 bg-rose-50 dark:bg-rose-100'} >
-                    <WarningAmberIcon sx={{fontSize: '65px'}} />
+                <DialogTitle className={'text-center text-rose-900 dark:text-rose-400'}>
+                    <WarningAmberIcon sx={{fontSize: '65px'}}/>
                 </DialogTitle>
 
-                <DialogContent className={'bg-rose-50 dark:bg-rose-100'}>
+                <DialogContent>
 
                     {/*<h2 className={'text-2xl font-bold text-center text-rose-900 bg-rose-50 dark:bg-rose-100'}>DELETE Idea</h2>*/}
 
                     <DialogContentText sx={{textAlign: 'center'}} id="alert-dialog-slide-description">
-                        Are you sure you want to delete the idea:
-                        <span className="italic text-gray-700"> "{idea.title}" </span>?
+                            Are you sure you want to delete the idea:
+                            <span className="italic"> "{idea.title}" </span>?
                     </DialogContentText>
 
                 </DialogContent>
 
-                <DialogActions className={'bg-rose-50 dark:bg-rose-100'}>
+                <DialogActions>
                     <Button onClick={handleCloseDeleteModal}>Cancel</Button>
-                    <Button color="error" onClick={()=>deleteIdea(idea.id)}>DELETE</Button>
+                    <Button color="error" onClick={() => deleteIdea(idea.id)}>DELETE</Button>
                 </DialogActions>
             </Dialog>
 
@@ -94,19 +95,22 @@ function IdeaCard({idea}) {
                 fullScreen
                 TransitionComponent={Transition}
                 open={openViewIdeaModal}
-                onClose={()=>setOpenViewIdeaModal(false)}
+                onClose={() => setOpenViewIdeaModal(false)}
             >
                 <DialogTitle className={'bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-gray-300'}>
                     <TipsAndUpdatesIcon/> {idea.title}</DialogTitle>
-                    <Button sx={{position:'absolute',
-                        top:'10px',
-                        right: '10px', display: {xs: 'none', md: 'block'}}}
-                            onClick={()=>setOpenViewIdeaModal(false)}><CloseIcon/></Button>
+                <Button sx={{
+                    position: 'absolute',
+                    top: '10px',
+                    right: '10px', display: {xs: 'none', md: 'block'}
+                }}
+                        onClick={() => setOpenViewIdeaModal(false)}><CloseIcon/></Button>
                 <DialogContent className={'bg-white dark:bg-gray-700'}>
 
-                        <h4 className={'text-xl text-gray-700 dark:text-gray-300 border-b'}><TextSnippetIcon/> Idea Description</h4>
+                    <h4 className={'text-xl text-gray-700 dark:text-gray-300 border-b'}><TextSnippetIcon/> Idea
+                        Description</h4>
                     <DialogContentText className={'pt-3 text-gray-700 dark:text-gray-400'}>
-                            {idea.description}
+                        {idea.description}
                     </DialogContentText>
 
                 </DialogContent>
@@ -117,7 +121,7 @@ function IdeaCard({idea}) {
                         ? (
                             <div className={'p-3 border-b text-gray-700 dark:text-gray-400'}>
                                 <span className={'font-semibold'}><SportsScoreIcon/> GOAL: </span>
-                                <NavLink to={'/goals/'+idea.goalId}
+                                <NavLink to={'/goals/' + idea.goalId}
                                          className={'inline hover:underline hover:text-cyan-700 dark:hover:text-cyan-100'}>
                                     {idea.goalTitle}
                                 </NavLink>
@@ -126,7 +130,7 @@ function IdeaCard({idea}) {
                         : ''
                     }
 
-                    <Button sx={{display: {md: 'none'}}} onClick={()=>setOpenViewIdeaModal(false)}>
+                    <Button sx={{display: {md: 'none'}}} onClick={() => setOpenViewIdeaModal(false)}>
                         <CloseIcon/>
                     </Button>
 
